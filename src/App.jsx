c3156c021f366dd4e6e0946e3d278ef89e766a01
Navarroseb <sebastnavarro@gmail.com>
2023-12-12 16:12:14 -0300

    Starting Sidebar mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine  } from "react-icons/ri";
 import Sidebar from "./components/shared/Sidebar"
 
 
 function App() {
- 
+
   return (
     <>
       <div className="bg-[#262837] w-full min-h-screen" >
         <Sidebar />
+        {/* Menú Movil */}
+        <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-4 px-8 flex items-center justify-between rounded-tl-xl roundedtr-xl">
+          <button>
+            <RiUser3Line />
+          </button>
+          <button>
+            <RiAddLine />
+          </button>
+          <button>
+            <RiPieChartLine />
+          </button>
+          <button className="text-white">
+            <RiMenu3Fill />
+          </button>
+        </nav>
       </div>
     </>
   )
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -2,7 +2,7 @@ import { RiHome6Line, RiPercentLine, RiPieChartLine, RiMailLine, RiNotification4
 
 const Sidebar = () => {
     return (
-        <div className='bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl'>
+        <div className='bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 -left-full'>
             <div>
                 <ul className="pl-4">
                     <li>
